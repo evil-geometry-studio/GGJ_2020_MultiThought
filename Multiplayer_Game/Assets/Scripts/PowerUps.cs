f0c03aa6580c0511d24d1d0c2dde7061be601972
AbrahamGarzon <thesnowboox@gmail.com>
2020-02-01 20:18:07 -0600

    PowerUpsParte1
    Script de la creacion de power ups en fase alfa

---
+++ Multiplayer_Game/Assets/Scripts/PowerUps.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUps : MonoBehaviour
+{
+    public enum TypePowerUp {PVel, Iman};
+    public TypePowerUp PowerType;
+
+    public PlayerController p1, p2;
+    public LightController reference;
+
+
+    void Start()
+    {
+        p1 = GameObject.FindGameObjectWithTag("Player1").GetComponent<PlayerController>();
+        p2 = GameObject.FindGameObjectWithTag("Player2").GetComponent<PlayerController>();
+
+        reference = GameObject.FindGameObjectWithTag("Player1").GetComponent<LightController>();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player1") && PowerType == TypePowerUp.PVel)
+        {
+            Debug.Log("Entre1");
+            StartCoroutine("Velocidad1");
+        }
+
+        if(collision.gameObject.CompareTag("Player2") && PowerType == TypePowerUp.PVel)
+        {
+            StartCoroutine("Velocidad2");
+            Debug.Log("Entre2");
+        }
+
+        if (collision.gameObject.CompareTag("Player1") && PowerType == TypePowerUp.Iman)
+        {
+            reference.iman = true;
+            reference.playerPosition = p1.gameObject.transform.position;
+            Debug.Log("Entre3");
+        }
+        if (collision.gameObject.CompareTag("Player2") && PowerType == TypePowerUp.Iman)
+        {
+            reference.iman = true;
+            reference.playerPosition = p2.gameObject.transform.position;
+            Debug.Log("Entre4");
+        }
+    }
+    IEnumerator Velocidad1()
+    {
+        p1.Speed += 1f;
+        p2.Speed -= 1f;
+        yield return new WaitForSeconds(1.5f);
+        p1.Speed -= 1f;
+        p2.Speed += 1f;
+    }
+    IEnumerator Velocidad2()
+    {
+        p1.Speed -= 1f;
+        p2.Speed += 1f;
+        yield return new WaitForSeconds(1.5f);
+        p1.Speed += 1f;
+        p2.Speed -= 1f;
+    }
+}
